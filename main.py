0023fc11a70c4959465a6045d59f5d435a5968d0
코야 <66289470+coyaSONG@users.noreply.github.com>
2022-08-13 14:48:09 +0000

    남은 인생 계산기

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 # 🚨 Don't change the code below 👇
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
+age = input("What is your current age?")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-bmi = float(weight) / float(height)**2
-print(int(bmi))
+age_int = int(age)
+my_year = 90 - age_int
+val_1 = my_year * 365
+val_2 = my_year * 52
+val_3 = my_year * 12
+print(f"You have {val_1} days, {val_2} weeks and {val_3} months left.")
+
+
+
+
+
+
+
+
